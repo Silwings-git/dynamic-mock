d4e5319a08b7b7d046c60ef7da0e2f97e9558a11
Silwings <1910506332@qq.com>
2022-11-09 01:05:21 +0800

    pom

---
+++ dynamic-mock-admin/src/main/java/top/silwings/admin/DynamicMockAdminApplication.java
@@ -0,0 +1,13 @@
+package top.silwings.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicMockAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicMockAdminApplication.class, args);
+    }
+
+}
