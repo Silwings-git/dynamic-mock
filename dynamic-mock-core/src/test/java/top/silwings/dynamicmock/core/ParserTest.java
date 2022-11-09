4732742513af4976c72de7aa14ca50cf69575d67
Silwings <1910506332@qq.com>
2022-11-09 21:46:35 +0800

    ParserJmh

--- dynamic-mock-core/src/test/java/top/silwings/dynamicmock/core/ParserTest.java
+++ dynamic-mock-core/src/test/java/top/silwings/dynamicmock/core/ParserTest.java
@@ -1,11 +1,17 @@
 package top.silwings.dynamicmock.core;
 
 import com.alibaba.fastjson.JSON;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+import top.silwings.core.Context;
+import top.silwings.core.JsonNodeParser;
 import top.silwings.core.ParameterContext;
 import top.silwings.core.dynamic.DynamicValue;
 import top.silwings.core.dynamic.DynamicValueFactory;
+import top.silwings.core.node.Node;
 
+@Slf4j
 public class ParserTest {
 
     @Test
@@ -43,6 +49,40 @@ public class ParserTest {
         context.putParameter("10", "御坂美琴");
 
         System.out.println(JSON.toJSONString(dynamicValue.value(context)));
+    }
+
+    @Test
+    public void test002() {
+
+        final TestData testData = new TestData();
+
+        final Node analyze1 = testData.getJsonNodeParser().parse(testData.getTest002());
+
+        final Context context = new Context(testData.getTest002().length(), testData.getParameterContext());
+
+        analyze1.interpret(context);
+
+        log.info(context.getJsonStr());
+    }
+
+    @Getter
+    public static class TestData {
+        private final String test001 = "#search(#search(#search(#search(param)+(20-#search(paramA)--1-2))))";
+        private final String test002 =  "{\n" +
+                "\"id\":\"${#uuid(abc)}\"," +
+                "\"name\": \"${#search(#search(3*(1+1)--2-6))}\"," +
+                "\"age\": \"${#search(age)}\"," +
+                "\"happy\": \"${             10  + 1-1        == 11-1 }\"" +
+                "}";
+        private final JsonNodeParser jsonNodeParser = new JsonNodeParser(new DynamicValueFactory());
+        private final ParameterContext parameterContext = new ParameterContext();
+
+        public TestData() {
+            this.parameterContext.putParameter("param", 1);
+            this.parameterContext.putParameter("2", "name");
+            this.parameterContext.putParameter("name", "御坂美琴");
+            this.parameterContext.putParameter("age", 14);
+        }
     }
 
 }
