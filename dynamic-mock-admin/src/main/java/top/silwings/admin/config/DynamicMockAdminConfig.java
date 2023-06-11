b95a21a2f3e0bc01fdc9dab646a2c1601be26504
Silwings <1910506332@qq.com>
2023-06-11 21:33:57 +0800

    DynamicMockAdminConfig

--- dynamic-mock-admin/src/main/java/top/silwings/admin/config/DynamicMockAdminConfig.java
+++ dynamic-mock-admin/src/main/java/top/silwings/admin/config/DynamicMockAdminConfig.java
@@ -1,7 +1,6 @@
 package top.silwings.admin.config;
 
 import io.swagger.annotations.Api;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
