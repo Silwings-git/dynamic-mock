104346ad32d80dd5cc2c9da916ab3ce873fb1946
Silwings <1910506332@qq.com>
2023-06-11 22:50:01 +0800

    PluginRegistrationProgram

--- dynamic-mock-core/src/main/java/top/silwings/core/handler/plugin/PluginRegistrationProgram.java
+++ dynamic-mock-core/src/main/java/top/silwings/core/handler/plugin/PluginRegistrationProgram.java
@@ -3,8 +3,8 @@ package top.silwings.core.handler.plugin;
 import top.silwings.core.model.MockHandlerDto;
 
 /**
- * @ClassName ScriptRegistrationProgram
- * @Description 脚本注册程序
+ * @ClassName PluginRegistrationProgram
+ * @Description 插件注册程序
  * @Author Silwings
  * @Date 2023/6/11 20:02
  * @Since
