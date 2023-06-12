3f91353e0cd9e782bef6abfd9b5d3093a4293c2f
Silwings <1910506332@qq.com>
2023-06-12 13:16:55 +0800

    ClosedException

---
+++ dynamic-mock-core/src/main/java/top/silwings/core/exceptions/ClosedException.java
@@ -0,0 +1,14 @@
+package top.silwings.core.exceptions;
+
+/**
+ * @ClassName ClosedException
+ * @Description 已关闭异常
+ * @Author Silwings
+ * @Date 2023/6/12 13:14
+ * @Since
+ **/
+public class ClosedException extends BaseDynamicMockException{
+    public ClosedException(final String message) {
+        super(message);
+    }
+}
--- dynamic-mock-core/src/main/java/top/silwings/core/handler/plugin/PluginExecutorManager.java
+++ dynamic-mock-core/src/main/java/top/silwings/core/handler/plugin/PluginExecutorManager.java
@@ -1,6 +1,7 @@
 package top.silwings.core.handler.plugin;
 
 import lombok.extern.slf4j.Slf4j;
+import top.silwings.core.exceptions.ClosedException;
 import top.silwings.core.handler.plugin.executors.PluginExecutor;
 import top.silwings.core.handler.plugin.interfaces.Ordered;
 
@@ -23,20 +24,30 @@ import java.util.Map;
 @Slf4j
 public class PluginExecutorManager implements Closeable {
 
+    private volatile boolean closed = false;
     private final Map<PluginInterfaceType, List<PluginExecutor<?>>> scriptLanguageScriptMap = new EnumMap<>(PluginInterfaceType.class);
 
     public List<PluginExecutor<?>> getExecutors(final PluginInterfaceType pluginInterfaceType) {
+        this.validateClose();
         return this.scriptLanguageScriptMap.getOrDefault(pluginInterfaceType, Collections.emptyList());
     }
 
     public void register(final PluginExecutor<?> pluginExecutor) {
+        this.validateClose();
         final List<PluginExecutor<?>> pluginExecutorList = this.scriptLanguageScriptMap.computeIfAbsent(pluginExecutor.getPluginInterfaceType(), key -> new ArrayList<>());
         pluginExecutorList.add(pluginExecutor);
         pluginExecutorList.sort(Comparator.comparingInt(Ordered::getOrder));
     }
 
+    private void validateClose() {
+        if (this.closed) {
+            throw new ClosedException("PluginExecutorManager is closed!");
+        }
+    }
+
     @Override
     public void close() {
+        this.closed = true;
         this.scriptLanguageScriptMap.values().forEach(pluginExecutors -> pluginExecutors.forEach(pluginExecutor -> {
             try {
                 pluginExecutor.close();
